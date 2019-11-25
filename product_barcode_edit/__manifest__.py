b1491ede9abf88ff515d1f91885596be3b7b7c37
eman <emanahmed4689@gmail.com>
2019-11-25 15:50:42 +0200

    [Add] new module product_barcode_edit

---
+++ product_barcode_edit/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product Barcode Edit",
+
+    'summary': """
+       Product Barcode Edit """,
+
+    'description': """
+        Product Barcode Edit
+    """,
+
+    'author': "ITsys-Corportion Doaa",
+    'website': "http://www.it-syscorp.com",
+
+    'category': 'Product',
+    'version': '0.1',
+
+    'depends': ['base','product'],
+
+    'data': [
+        'views/product_barcode_report.xml',
+    ],
+
+}
